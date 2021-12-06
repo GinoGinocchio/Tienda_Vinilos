9517227b1fc03eb3ae02862f7be9678b6a4d87c7
Edson <edsonsalvador2014@gmail.com>
2021-12-06 02:17:02 -0500

    Some css errors

--- tienda_vinilos/tienda_vinilos/settings.py
+++ tienda_vinilos/tienda_vinilos/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = 'dist/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    # This defines a prefix so the url paths will become `/static/node_modules/...`
+    ('node_modules', os.path.join(BASE_DIR, 'node_modules/')),
+)
 
 import os
 
